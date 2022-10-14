742022f7c2557384718156727c6acfbbfeef7405
Carter Matschek <carter.matschek@gmail.com>
2022-10-13 23:14:17 -0500

    created table and added date to jumbotron

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+//WHEN I open the planner
+//THEN the current day is displayed at the top of the calendar
+var currentDay = moment();
+$("#currentDay").text(currentDay.format("dddd, MMMM D, YYYY"));
+
+// function with another inside with a click event for the save button
+// then save that
+
+// /second function to update the hours
+
+// setinterval to check to see if it's needed to change
+
+// local storage
